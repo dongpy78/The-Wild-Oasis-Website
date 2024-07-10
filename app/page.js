e30b5002ef262b37af26ad073bb3ced66f8a505f
Van Dong <vandong78py@gmail.com>
2024-07-10 08:35:27 +0700

    creating a layout

--- app/page.js
+++ app/page.js
@@ -4,8 +4,6 @@ import Navigation from "./components/Navigation";
 export default function Page() {
   return (
     <div>
-      <Navigation />
-
       <h1>Hello Next JS</h1>
       <Link href="/cabins">Explore luxury cabins</Link>
     </div>
