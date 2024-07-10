348f48eff1d9439c1dcf2a47d2b32ff72eda132d
Van Dong <vandong78py@gmail.com>
2024-07-10 07:56:07 +0700

    navigating between pages

--- app/page.js
+++ app/page.js
@@ -1,3 +1,13 @@
+import Link from "next/link";
+import Navigation from "./components/Navigation";
+
 export default function Page() {
-  return <h1>Hello Next JS</h1>;
+  return (
+    <div>
+      <Navigation />
+
+      <h1>Hello Next JS</h1>
+      <Link href="/cabins">Explore luxury cabins</Link>
+    </div>
+  );
 }
